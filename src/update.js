8b63a30aaf8d4ba4140bba3684b22097204d87b2
ahkui <ahkui@outlook.com>
2020-11-01 16:39:41 +0800

    update

--- src/update.js
+++ src/update.js
@@ -19,79 +19,85 @@ exports.builder = function (yargs) {
 }
 
 exports.handler = async function (argv) {
-    await show();
-
-    const localModList = (await readModList());
-    const mods = Object.keys(localModList).map(key => {
-        const mod = localModList[key];
-        mod.key = key;
-        return mod
-    })
-
-    let currentPage = 0;
-    let selectIndex = ''
-
-    // Select Mod
     while (true) {
-        selectIndex = ''
-        while (selectIndex == '' || selectIndex < 0 || selectIndex >= mods.length)
-            selectIndex = await ask("Select mod(index): ");
+        await show();
 
-        if (mods[selectIndex] == undefined)
-            continue
-        break
+        const localModList = (await readModList());
+        const mods = Object.keys(localModList).map(key => {
+            const mod = localModList[key];
+            mod.key = key;
+            return mod
+        })
+
+        let currentPage = 0;
+        let selectIndex = ''
+
+        // Select Mod
+        while (true) {
+            selectIndex = ''
+            while (selectIndex == '' || selectIndex < 0 || selectIndex >= mods.length) {
+                selectIndex = await ask("Select mod(index; 'q' to exit): ")
+
+                if (selectIndex == 'q')
+                    return
+            }
+
+            if (mods[selectIndex] == undefined)
+                continue
+            break
+        }
+
+        const selectedMod = mods[selectIndex]
+
+        const files = (await curseforge.getModFiles(selectedMod.mod_id)).sort((a, b) => {
+            const left = new Date(b.timestamp);
+            const right = new Date(a.timestamp);
+
+            if (left < right) {
+                return -1;
+            }
+            if (left > right) {
+                return 1;
+            }
+
+            return 0;
+        }).chunk(10)
+        currentPage = 0
+
+        while (true) {
+            selectIndex = '';
+            while (selectIndex == '') {
+                console.clear()
+                console.log(`Current mod: ${selectedMod.name}`)
+                console.table(parsedFileList(files[currentPage]))
+                console.log(`Page: ${currentPage + 1}/${files.length} (Tips: \`b\` prev page, \`n\` next page)`);
+
+                selectIndex = await ask("Select file(index): ");
+            }
+
+            if (selectIndex == 'b') {
+                if (currentPage > 0)
+                    currentPage -= 1;
+                continue
+            } else if (selectIndex == 'n') {
+                console.log(currentPage < files.length, currentPage, files.length)
+                if (currentPage < files.length - 1)
+                    currentPage += 1;
+                continue
+            }
+
+            if (files[currentPage][selectIndex] == undefined)
+                continue
+
+            break
+        }
+        const selectedFile = files[currentPage][selectIndex]
+
+        localModList[selectedMod.key].file_id = selectedFile.id
+        localModList[selectedMod.key].file_url = selectedFile.download_url
+
+        await writeModList(localModList);
+
+        console.log('Done!')
     }
-
-    const selectedMod = mods[selectIndex]
-
-    const files = (await curseforge.getModFiles(selectedMod.mod_id)).sort((a, b) => {
-        const left = new Date(b.timestamp);
-        const right = new Date(a.timestamp);
-
-        if (left < right) {
-            return -1;
-        }
-        if (left > right) {
-            return 1;
-        }
-
-        return 0;
-    }).chunk(10)
-    currentPage = 0
-
-    while (true) {
-        selectIndex = '';
-        while (selectIndex == '') {
-            console.clear()
-            console.log(`Current mod: ${selectedMod.name}`)
-            console.table(parsedFileList(files[currentPage]))
-            console.log(`Page: ${currentPage + 1}/${files.length} (Tips: \`b\` prev page, \`n\` next page)`);
-
-            selectIndex = await ask("Select file(index): ");
-        }
-
-        if (selectIndex == 'b') {
-            if (currentPage > 0)
-                currentPage -= 1;
-            continue
-        } else if (selectIndex == 'n') {
-            console.log(currentPage < files.length, currentPage, files.length)
-            if (currentPage < files.length - 1)
-                currentPage += 1;
-            continue
-        }
-
-        if (files[currentPage][selectIndex] == undefined)
-            continue
-
-        break
-    }
-    const selectedFile = files[currentPage][selectIndex]
-
-    localModList[selectedMod.key].file_id = selectedFile.id
-    localModList[selectedMod.key].file_url = selectedFile.download_url
-
-    await writeModList(localModList);
-
-    console.log('Done!')
 }
--- src/utils/checkIsLatest.js
+++ src/utils/checkIsLatest.js
@@ -1,7 +1,10 @@
 const curseforge = require("mc-curseforge-api");
 
+let data = {};
+
 module.exports = async function ({ modId, currentFileId }) {
-    const mod = await curseforge.getMod(modId)
+    const mod = data[modId] || await curseforge.getMod(modId)
+    data[modId] = mod;
     for (const file of mod.latestFiles) {
         const { id } = file;
         if (id == currentFileId) return true;
